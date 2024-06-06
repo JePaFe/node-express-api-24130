b407b3731b37451f7bd3ebf02bb5a005c390b81a
Jean Paul Ferreira <jepafe@gmail.com>
2024-06-05 22:37:27 -0300

    Archivos estáticos y rutas

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
+// Middleware para archivos estáticos y públicos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal, la pagina de inicio
 // http://localhost:3000/
 app.get("/", (req, res) => {
   res.send("Hola Express");
+});
+
+// Esta ruta es para mostrar un archivo en una carpeta privada
+// Puedo mas adelante controlar quien tiene acceso
+app.get("/factura", (req, res) => {
+  // login
+  res.sendFile(path.join(__dirname, "private", "factura.html"));
+});
+
+// En esta ruta se envia un archivo JSON
+app.get("/frutas", (req, res) => {
+  // Con el query se pueden enviar cadenas de consultas
+  // Ejemplo: /frutas?order=nombre&limit=5
+  console.log(req.query);
+  res.sendFile(path.join(__dirname, "frutas.json"));
+});
+
+app.get("/frutas/:id", (req, res) => {
+  // Con los params se puede obtener parámetros definidos en la ruta
+  console.log(req.params.id);
+  res.send("Una fruta con el id: " + req.params.id);
 });
 
 const PORT = 3000;
