a3547277da8d9ef5f6355ff6126eefba5c6055ff
Jean Paul Ferreira <jepafe@gmail.com>
2024-06-19 22:56:09 -0300

    Node 06 - Multer

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -1,5 +1,8 @@
 const db = require("../db/db");
 
+const fs = require("fs");
+const path = require("path");
+
 const index = (req, res) => {
   const sql = "SELECT * FROM productos";
   db.query(sql, (error, rows) => {
@@ -24,6 +27,8 @@ const show = (req, res) => {
       return res.status(500).json({ error: "Intente mas tarde" });
     }
 
+    
+
     if (rows.length === 0) {
       return res.status(404).json({ message: "No existe el producto" });
     }
@@ -33,13 +38,18 @@ const show = (req, res) => {
 };
 
 const store = (req, res) => {
+  // console.log(req.file);
+
+  const { filename } = req.file;
   const { nombre, precio, stock } = req.body;
 
-  const sql = "INSERT INTO productos (nombre, precio, stock) VALUES (?, ?, ?)";
-  db.query(sql, [nombre, precio, stock], (error, result) => {
-    console.log(result);
+  const sql =
+    "INSERT INTO productos (nombre, precio, stock, imagen) VALUES (?, ?, ?, ?)";
+  db.query(sql, [nombre, precio, stock, filename], (error, result) => {
+    // console.log(result);
     if (error) {
-      console.log(error);
+      // console.log(error);
+      fs.unlinkSync(path.join(__dirname, "../public/uploads", filename));
       return res.status(500).json({ error: "Intente mas tarde" });
     }
 
@@ -52,20 +62,40 @@ const store = (req, res) => {
 };
 
 const update = (req, res) => {
+  // console.log(req.file);
+
+  let sql =
+    "UPDATE productos SET nombre = ?, stock = ?, precio = ? WHERE id = ?";
+
   const { id } = req.params;
   const { nombre, precio, stock } = req.body;
 
-  const sql =
-    "UPDATE productos SET nombre = ?, stock = ?, precio = ? WHERE id = ?";
-  db.query(sql, [nombre, stock, precio, id], (error, result) => {
+  const values = [nombre, stock, precio];
+
+  if (req.file) {
+    const { filename } = req.file;
+    sql =
+      "UPDATE productos SET nombre = ?, stock = ?, precio = ?, imagen = ? WHERE id = ?";
+    values.push(filename);
+  }
+
+  values.push(id);
+
+  db.query(sql, values, (error, result) => {
     // console.log(result);
     if (error) {
       console.log(error);
+      // Borra imagen subida
       return res.status(500).json({ error: "Intente mas tarde" });
     }
 
     if (result.affectedRows === 0) {
+      // Borra imagen subida
       return res.status(404).json({ message: "No existe el producto" });
+    }
+
+    if (result.affectedRows === 1) {
+      // fs.unlink a la imagen anterior
     }
 
     const producto = { ...req.body, ...req.params };
